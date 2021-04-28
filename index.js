c72d791c6acfda32ef861c6944db6892ca016d56
SabrenaMichelle <sabrenaray@yahoo.com>
2021-04-28 09:35:15 -0400

    Finished

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const drivers = ["Milo","Otis", "Garfield"];
+
+function destructivelyAppendDriver(){
+    drivers.push('Ralph')
+}
+function destructivelyPrependDriver(){
+    drivers.unshift('Bob')
+}
+function destructivelyRemoveLastDriver(){
+    drivers.pop()
+}
+function destructivelyRemoveFirstDriver(){
+    drivers.shift()
+}
+function appendDriver(){
+    const newDrivers = [...drivers, "Broom"]
+    return newDrivers
+}
+function prependDriver(){
+    const newDrivers = ["Arnold", ...drivers]
+    return newDrivers
+}
+function removeLastDriver(){
+    const removesDriver = drivers.slice(0, drivers.length -1)
+    return removesDriver
+}
+function removeFirstDriver(){
+    const removesDriver = drivers.slice(1)
+    return removesDriver
+}//why would drivers.slice(0) remove the driver?
